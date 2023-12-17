0078e79092c6ceddd9b3377f049b59ba37f4c89a
Umut Şar <contact@umutsar.net>
2023-12-17 09:30:35 +0300

    Create ChangePage.js

---
+++ ChangePage.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>First Page</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('SecondPage')}
+      >
+        <Text style={styles.buttonText}>Go to the second page</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const SecondScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Second Page</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SecondPage" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
